b1a9957c4efeccfc921c39b682e0bce99c31ec58
developer-jesus-github <developerjesus@hotmail.com>
2020-11-30 17:44:36 -0600

    feat: Frontend is now making requests to the back-end.

--- contenedor-f/src/components/screens/Comments.js
+++ contenedor-f/src/components/screens/Comments.js
@@ -11,35 +11,34 @@ export default class Comments extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      playlist: [],
+      comments: [],
       loading: true,
-      id: this.props.location.carouselId,
+      id: this.props.location.id,
+      carouselId: this.props.location.carouselId,
     };
   }
 
   async componentDidMount(){
     console.log(this.state.id)
-    await this.getPlaylist();
+    await this.getComments();
   }
 
-  /* Fetch the playlist and render a carousel with songs. */
-  getPlaylist = async () => {
-    const request = await fetch('https://vocadb.net/api/songs', 
+  /* Fetch the comments list and render a carousel with comments. */
+  getComments = async () => {
+    const request = await fetch(`http://nodeapi:1337/songs/${this.state.id}/comments`, 
       {
         headers: {
           "Accept" : "application/json",
         },
       }
     );
-    const playlist = await request.json();
+    const comments = await request.json();
     /* If there is data then render it. */
-    if(playlist){
-      let json = [{"created":"2016-04-18T20:22:11Z","message":"Well, the album entry says that most songs are remastered, so they sound a bit different. Sometimes, if the remaster is different enough, it makes sense to have another entry for the remastered version.\n\nAre you saying you want to *buy* the original version as mp3, or why are you looking for it? I'd check the other albums this song is featured on, maybe the older version is on some other album.","author":{"name":"riipah","picture":"https://www.gravatar.com/avatar/2f5d563125cdb6d78a300862ff60f9f1?s=80","verified":false,"memberSince":"2011-07-10T18:17:44","active":true,"group":"Trusted"}},{"created":"2016-04-18T20:14:06Z","message":"I've checked out the version of Rolling Girl in Unhappy Refrain (iTunes ver.), and it sounds different to the original song on NND. Where can I find the original version?","author":{"name":"Delta_86","picture":"https://www.gravatar.com/avatar/923c1e9423dbc2ce6c8c4af509ef3019?s=80","verified":false,"memberSince":"2016-02-26T04:24:03","active":true,"group":"Regular"}},{"created":"2015-05-28T09:07:56Z","message":"Actually that's not true. Nico embeds work just fine even if you're not logged in (works here too).","author":{"name":"riipah","picture":"https://www.gravatar.com/avatar/2f5d563125cdb6d78a300862ff60f9f1?s=80","verified":false,"memberSince":"2011-07-10T18:17:44","active":true,"group":"Trusted"}},{"created":"2015-05-27T23:31:27Z","message":"To the anon user who reported the PV as broken, you either have to click on the YT link to view it embeded, or create an NND account, you can't view NND embeds it if you're not logged to NND, or create an account on vocadb.net and make YT your default choice to view embeds.","author":{"name":"zazuge","picture":"https://www.gravatar.com/avatar/d7e612be8a3aa48c1337f26d0770f16a?s=80","verified":false,"memberSince":"2013-05-02T00:43:24","active":true,"group":"Trusted"}}];
+    if(comments){
       this.setState({
-        playlist: json,
+        comments: comments['comments'],
         loading: false,
       });
-      console.log(json);
     }else{
       alert('There are no comments...');
     }
@@ -48,13 +47,13 @@ export default class Comments extends React.Component{
   render(){
     return (
       <div>
-        <Navbar title="Comments" back={this.state.id}></Navbar>
+        <Navbar title="Comments" back={this.state.carouselId}></Navbar>
         <Container maxWidth="md">
           <Grid container direction="row" justify="center" alignItems="center">
             {this.state.loading &&
               <Loader></Loader>
             }
-            {this.state.playlist.map((res, i) => (
+            {this.state.comments.map((res, i) => (
               <Grid item xs={12} sm={6} md={4}>
                 <CardComment data={res}></CardComment>
               </Grid>
