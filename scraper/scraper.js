803980383214d89bfc68b6e4cc69d02f3c1638cc
David Aguirre <daguirrez2000@gmail.com>
2020-11-30 11:51:53 -0600

    feat: Insert into mongo DB & scraper dockerized.

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,3 +1,4 @@
+const { MongoDB }	= require("./modules/db");
 const getSongs		= require("./modules/songs");
 const getComments	= require("./modules/comments");
 const getLyrics		= require("./modules/lyrics");
@@ -8,10 +9,13 @@ const getRatings	= require("./modules/ratings");
 	var songs = await getSongs(); // get songs
 
 	songs.forEach(async s => {
+		// get individual data
 		s.comments	= await getComments(s.id);
 		s.lyrics	= await getLyrics(s.id);
 		s.ratings	= await getRatings(s.id);
 
-		console.log(s); // INSERT INTO MONGO DB
+		// insert into DB
+		let db = new MongoDB();
+		db.saveSong(s); // save song
 	});
 })();
