3b2dd1383b1694b74ef553db4547c50946a44143
David Aguirre <daguirrez2000@gmail.com>
2020-11-30 04:27:26 -0600

    feat: Scraper async call structuring and added mongodb dependencies.

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,6 +1,17 @@
-const getSongs = require("./modules/songs");
+const getSongs		= require("./modules/songs");
+const getComments	= require("./modules/comments");
+const getLyrics		= require("./modules/lyrics");
+const getRatings	= require("./modules/ratings");
 
 // init scrapper
 (async () => {
-	console.log(await getSongs()); // get songs
+	var songs = await getSongs(); // get songs
+
+	songs.forEach(async s => {
+		s.comments	= await getComments(s.id);
+		s.lyrics	= await getLyrics(s.id);
+		s.ratings	= await getRatings(s.id);
+
+		console.log(s); // INSERT INTO MONGO DB
+	});
 })();
