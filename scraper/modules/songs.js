fe29619d0928da203cf87ff7d9a2b039ff3c62f9
David Aguirre <daguirrez2000@gmail.com>
2020-11-30 02:00:06 -0600

    feat: Added get songs by artist & highlighted, top-rated songs.

--- scraper/modules/songs.js
+++ scraper/modules/songs.js
@@ -1,27 +1,37 @@
 const fetch = require("./fetch");
 
-module.exports = async () => {
-	let songs = await fetch(
-		"https://vocadb.net/api/songs",
-		{
-			songTypes: "Original",
-			includeMembers: true,
-			onlyWithPvs: true,
-			pvServices: "Youtube",
-			maxResults: 20,
-			sort: "RatingScore",
-			fields: "PVs,MainPicture"
-		}
-	);
+const getSongs = async (url, defv, params = {}) => {
+	let songs = await fetch(url, params); // fetch songs
 
-	if (!songs) return [];
+	return !!songs ? songs : defv;
+}
 
-	// filter API data
+const getArtistSongs = async (url, artists, params) => {
+	let songs = [];
+	
+	for(let a of artists) { // get each artist songs
+		songs = songs.concat((await getSongs(
+			url,
+			{ items: [] },
+			{
+				artistId: a,
+				artistParticipationStatus: "OnlyMainAlbums",
+				... params
+			}
+		)).items);
+		
+	}
+
+	return songs;
+}
+
+const getData = (songs, type) => {
 	let data = [];
-	for(let s of songs.items) {
+
+	songs.forEach(s => {
 		let pv = s.pvs.find(p => p.service == "Youtube");
 		if(!pv) // service not found
-			continue;
+			return;
 
 		data.push({
 			id:					s.id,
@@ -30,6 +40,7 @@ module.exports = async () => {
 			duration:			s.lengthSeconds,
 			url:				pv.url,
 			picture:			s.mainPicture.urlThumb,
+			type:				type,
 			ranking: {
 				favorited:		s.favoritedTimes,
 				score:			s.ratingScore
@@ -39,7 +50,56 @@ module.exports = async () => {
 				artists:		s.artistString
 			}
 		});
-	}
+	});
 
-	return data; // return formatted data
+	return data; // get data
+}
+
+module.exports = async () => {
+	// get normal songs
+	let songs = await getArtistSongs(
+		"https://vocadb.net/api/songs",
+		[
+			45,			// DECO*27
+			23981,		// Yunosuke
+			885,		// Kairiki bear
+			24092,		// KIRA
+			10216,		// DIVELA
+			38			// 八王子P
+		],
+		{
+			songTypes:		"Original",
+			includeMembers: true,
+			onlyWithPvs:	true,
+			pvServices:		"Youtube",
+			maxResults:		4,
+			sort:			"PublishDate",
+			fields:			"PVs,MainPicture"
+		}
+	);
+
+	// get highlighted songs
+	let highlighteds = await getSongs(
+		"https://vocadb.net/api/songs/highlighted",
+		[],
+		{
+			fields: "PVs,MainPicture"
+		}
+	);
+
+	// get top rated songs
+	let topRated = await getSongs(
+		"https://vocadb.net/api/songs/top-rated",
+		[],
+		{
+			maxResults: 12,
+			fields: "PVs,MainPicture"
+		}
+	);
+
+	// return all songs with format
+	return getData(songs, "Normal")
+		.concat(getData(highlighteds, "Highlighted"))
+		.concat(getData(topRated, "TopRated"));
+
 }
